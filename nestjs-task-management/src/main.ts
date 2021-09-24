9887faa69b4253a37405fc401f69db07aacd70c5
Ashish Karki <ashishk@Ashishs-MacBook-Pro.local>
2021-09-24 17:56:10 +0545

    feat: Implemented validators for create-task.dto

--- nestjs-task-management/src/main.ts
+++ nestjs-task-management/src/main.ts
@@ -1,8 +1,14 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // tell Nestjs to run this Validator whenever it
+  // encounters our validation decorators like in create-task.dto
+  app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(3000);
 }
 bootstrap();
