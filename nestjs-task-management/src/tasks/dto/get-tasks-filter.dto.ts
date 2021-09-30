61202e7f561901ecd10e712bc1274f4849e03827
Ashish Karki <ashishk@Ashishs-MacBook-Pro.local>
2021-09-30 12:29:58 +0545

    feat: Implemented get-tasks flow

--- nestjs-task-management/src/tasks/dto/get-tasks-filter.dto.ts
+++ nestjs-task-management/src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,6 +1,13 @@
+import { IsEnum, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 import { TaskStatus } from './../task.model';
 
 export class GetTasksFilterDto {
+  @IsEnum(TaskStatus)
+  @IsOptional()
   status?: TaskStatus;
+
+  @IsNotEmpty()
+  @IsString()
+  @IsOptional()
   searchStr?: string;
 }
