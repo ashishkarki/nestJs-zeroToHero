866a42e8f799f2ddfaf4ffd0698e3072f444fab9
Ashish Karki <ashishk@Ashishs-MacBook-Pro.local>
2021-09-30 13:05:52 +0545

    feat: added connection params to postgres DB in app.module file

--- nestjs-task-management/.eslintrc.js
+++ nestjs-task-management/.eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: 'nestjs-task-management/tsconfig.json',
+    project: '**/**/tsconfig.json',
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
--- nestjs-task-management/src/app.module.ts
+++ nestjs-task-management/src/app.module.ts
@@ -1,7 +1,27 @@
+import {
+  POSTGRES_USERNAME,
+  POSTGRES_PASSWORD,
+  POSTGRES_TASKMGMT_DB_NAME,
+  HOST_NAME,
+  POSTGRES_PORT_NUM,
+} from './secrets/db-connection-creds';
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [TasksModule],
+  imports: [
+    TasksModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: HOST_NAME,
+      port: POSTGRES_PORT_NUM,
+      username: POSTGRES_USERNAME,
+      password: POSTGRES_PASSWORD,
+      database: POSTGRES_TASKMGMT_DB_NAME,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
