4265915331eb8989874f5e94d248735b11bb88ee
Ashish Karki <ashishk@Ashishs-MacBook-Pro.local>
2021-10-04 09:23:42 +0545

    feat: implemented signin feature

--- nestjs-task-management/src/auth/auth.service.ts
+++ nestjs-task-management/src/auth/auth.service.ts
@@ -1,7 +1,8 @@
 import { UserRepository } from './users.repository';
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -12,5 +13,19 @@ export class AuthService {
 
   signUp = async (authCredentialsDto: AuthCredentialsDto) => {
     return this.userRepo.createUser(authCredentialsDto);
+  };
+
+  signIn = async (authCredentialsDto: AuthCredentialsDto) => {
+    const { username, password } = authCredentialsDto;
+
+    // does this user exist ? but give the user any hint
+    const user = await this.userRepo.findOne({ username: username });
+
+    // compare the input 'password' with stored 'user.password'
+    if (user && (await bcrypt.compare(password, user.password))) {
+      return 'success';
+    } else {
+      throw new UnauthorizedException('Please check your login credentials!!');
+    }
   };
 }
